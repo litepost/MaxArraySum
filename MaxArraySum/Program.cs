b6c16c7f92542b5b58151f73405a98e22796df0a
litepost <litepost@gmail.com>
2024-03-24 19:59:57 -0700

    solved problem

--- MaxArraySum/Program.cs
+++ MaxArraySum/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.youtube.com/watch?v=n9F9wQD3Mhc
+// Find largest sum of non-adjacent numbers
+
+
+using System.Runtime.InteropServices;
+
+int FindMaxSubsetSum(List<int> input) {
+    if (input.Count == 0) return 0;
+    if (input.Count == 1) return input[0];
+
+    List<int> maxList = [];
+    
+    maxList.Add(input[0]);
+    maxList.Add(Math.Max(input[0], input[1]));
+
+    for (int i = 2; i < input.Count; i++) {
+        maxList.Add(Math.Max(Math.Max(input[i], input[i] + maxList[i-2]), maxList[i-1]));
+    }
+
+    return maxList.Last();
+}
+
+// List<int> entryArray = [3, 5, -7, 8, 10];
+// List<int> entryArray = [3];
+List<int> entryArray = [-7, 5, 10];
+
+Console.WriteLine(FindMaxSubsetSum(entryArray));
